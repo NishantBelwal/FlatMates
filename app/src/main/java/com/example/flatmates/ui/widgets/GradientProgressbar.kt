79fdfb87ccab283611953142f9c0f1f47f38b0b0
nishantbelwal12 <nishant.belwal@xperi.com>
2023-03-07 12:04:17 +0530

    Gradient Progress bar Util

---
+++ app/src/main/java/com/example/flatmates/ui/widgets/GradientProgressbar.kt
@@ -0,0 +1,68 @@
+package com.example.flatmates.ui.widgets
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GradientProgressbar(
+    indicatorHeight: Dp = 18.dp,
+    backgroundIndicatorColor: Color = Color.LightGray.copy(alpha = 0.3f),
+    indicatorPadding: Dp = 13.dp,
+    gradientColors: List<Color> = listOf(
+        Color(0xFF6ce0c4),
+        Color(0xFF40c7e7),
+        Color(0xFF6ce0c4),
+        Color(0xFF40c7e7)
+    ),
+    days: Float
+
+) {
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(indicatorHeight)
+            .padding(start = indicatorPadding, end = indicatorPadding)
+    ) {
+
+        var progress = 0f
+        if(days>30){
+            progress = (days/days) * size.width
+        }
+        else{
+            progress = (days/30) * size.width
+        }
+
+
+        drawLine(
+            color = backgroundIndicatorColor,
+            cap = StrokeCap.Round,
+            strokeWidth = size.height,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(x = size.width, y = 0f)
+        )
+
+        drawLine(
+            brush = Brush.linearGradient(
+                colors = gradientColors
+            ),
+            cap = StrokeCap.Round,
+            strokeWidth = size.height,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(x = progress, y = 0f)
+        )
+
+    }
+
+}
+
